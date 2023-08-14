ad4f9a92d9e861f48ca408b4b9d06155951f4663
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-08-14 22:56:25 +0300

    added JWT to log in and authenticate

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 const app = express();
-
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+
 
 //setup a connection to the mongo database.
 mongoose.connect(process.env.MONGO_URL);
@@ -16,7 +18,10 @@ mongoose.connect(process.env.MONGO_URL);
 const socialMediaUsers = new mongoose.Schema({
     username: String,
     email: String,
-    password: String
+    password: String,
+    bio: {type: String, default: ""},
+    gender: {type: String, default: ""}
+
 })
 
 // const socialMediaPosts = new mongoose.Schema({
@@ -26,14 +31,16 @@ const socialMediaUsers = new mongoose.Schema({
 const User = mongoose.model('socialMediaUser', socialMediaUsers);
 // const Posts = new mongoose.Model('socialMediaPost', socialMediaPosts);
 
-app.post("/users", (req, res)=>{
+app.post("/users/register", (req, res)=>{
     const { username, email, password } = req.body; //password is the user-input plain password.
+    //check if a user with that username already exists or not
     User.findOne({username: username})
     .then((user)=>{
         if (user){
-            console.log("User already exists.");
+            res.send("User already exists.");
         }
         else{
+            //if no such user exists, salt and hash the input password, and store the hash, email, username.
             bcrypt.hash(password, saltRounds)
             .then((hash)=>{
                     const newUser = new User({
@@ -42,18 +49,80 @@ app.post("/users", (req, res)=>{
                         password: hash
                     })
                     newUser.save().then(()=>{
-                        console.log("Successfully saved user")
+                        res.send("Successfully saved user")
                     })
-                    .catch(()=>{console.log("Some error occurred while saving user")})
+                    .catch(()=>{res.send("Some error occurred while saving user")})
                 })
-            .catch(err => console.log("Error: "+err));
+            .catch(err => res.send("Error: "+err));
         }
     })
-    .catch(err=>console.log(err));
-    res.send();
+    .catch(err=>res.send(err));
 })
 
+app.post("/users/login", (req, res)=>{
+    const {username, password} = req.body;
+    User.findOne({username})
+    .then((foundUser)=>{
+        if (!foundUser){
+            res.send("User does not exist.");
+        }
+        else{
+            bcrypt.compare(password, foundUser.password).then(function(result) {
+                if (result){
+                    const token = jwt.sign({ username: username }, process.env.SECRET_KEY);
+                    res.json({ token });
+                } else {
+                    res.send("Incorrect password!");
+                }
+            });  
+        }
+    })
 
+})
+
+//visit the profile of another user.
+app.get("/users/:username", (req, res)=>{
+    const username = req.params.username;
+    User.findOne({username: username})
+    .then((foundUser)=>{
+        console.log(foundUser);
+        if (foundUser){
+            res.send({
+                ...foundUser._doc,
+                password: "",
+                email: ""
+            })
+        }
+        else{
+            console.log("User doesn't exist.");
+            res.send();
+        }
+    })
+})
+
+//visiting own profile when logged in.
+app.post('/users/profile', authenticateToken, async (req, res) => {
+    try {
+      const user = await User.findOne({username: req.username});
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Error retrieving user profile' });
+    }
+  });
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
+    // console.log(token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) {return res.sendStatus(403)}
+      req.username = user.username
+      next()
+    })
+  }
+  
 
 app.listen(3000, function(){
     console.log(`Listening on port 3000`);
