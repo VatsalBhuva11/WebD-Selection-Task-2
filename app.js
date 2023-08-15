aaec9e56c6b96af94102e31e8ae4c007c4d9dd0d
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-08-15 14:36:24 +0300

    register user only for unique name and email

--- app.js
+++ app.js
@@ -1,256 +1,306 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express"); //for route handling
 const mongoose = require("mongoose"); //database
-const jwt = require('jsonwebtoken'); //authentication and authorization
-const cookieParser = require("cookie-parser") //storing JWT generated for authentication
+const jwt = require("jsonwebtoken"); //authentication and authorization
+const cookieParser = require("cookie-parser"); //storing JWT generated for authentication
 
-const multer = require('multer');
+const multer = require("multer");
 
-const bcrypt = require('bcrypt'); //securing passwords
+const bcrypt = require("bcrypt"); //securing passwords
 const saltRounds = 10;
 const PORT = process.env.PORT | 3000;
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 
 //using multer to handle storage of files (posts)
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Define the destination folder for uploaded files
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname); // Define the filename
-    },
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Define the destination folder for uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Define the filename
+  },
 });
 const upload = multer({ storage: storage });
 // app.use(express.json());
-
 
 //setup a connection to the mongo database.
 mongoose.connect(process.env.MONGO_URL);
 
 //a new schema to store the username, email, and hash of every user.
 const socialMediaUsers = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    bio: {type: String, default: ""},
-    gender: {type: String, default: ""}
-    
-})
+  username: String,
+  email: String,
+  password: String,
+  bio: { type: String, default: "" },
+  gender: { type: String, default: "" },
+});
 
 const Post = new mongoose.Schema({
-    imgPath: String,
-    caption: String,
-    likes: {type: Number, default: 0},
-    dislikes: {type: Number, default: 0},
-    comments: Array
-})
+  imgPath: String,
+  caption: String,
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  comments: Array,
+});
 
-//schema to store all the posts of a given user. the posts array contains an array of posts, where 
+//schema to store all the posts of a given user. the posts array contains an array of posts, where
 //each post is of a different schema that contains information about the likes, dislikes, comments on that post.
 const socialMediaPosts = new mongoose.Schema({
-    username: String,
-    posts: [Post]
-})
+  username: String,
+  posts: [Post],
+});
 
-const User = mongoose.model('socialMediaUser', socialMediaUsers);
-const singlePost = mongoose.model('singlePost', Post);
-const Posts = mongoose.model('socialMediaPost', socialMediaPosts);
+const User = mongoose.model("socialMediaUser", socialMediaUsers);
+const singlePost = mongoose.model("singlePost", Post);
+const Posts = mongoose.model("socialMediaPost", socialMediaPosts);
 
-app.post("/users/register", (req, res)=>{
-    const { username, email, password } = req.body; //password is the user-input plain password.
-    //check if a user with that username already exists or not
-    User.findOne({username: username})
-    .then((user)=>{
-        if (user){
-            res.send("User already exists.");
-        } else if (user.email === email) {
-            res.send("This email is already in use.");
+app.post("/users/register", (req, res) => {
+  const { username, email, password } = req.body; //password is the user-input plain password.
+  //check if a user with that username already exists or not
+  User.find({ $or: [
+    { username: username },
+    { email: email }
+  ] })
+    .then((users) => {
+      if (users.length !== 0) {
+        res.send("User already exists.");
+      } else {
+        //if no such user exists, salt and hash the input password, and store the hash, email, username.
+        bcrypt
+          .hash(password, saltRounds)
+          .then((hash) => {
+            const newUser = new User({
+              username: username,
+              email: email,
+              password: hash,
+            });
+            newUser
+              .save()
+              .then(() => {
+                new Posts({ username: username })
+                  .save()
+                  .then(() => {
+                    res.send("Successfully saved user");
+                  })
+                  .catch(() => {
+                    res.send("Error occurred while registering user.");
+                  });
+              })
+              .catch(() => {
+                res.send("Some error occurred while saving user");
+              });
+          })
+          .catch((err) => res.send("Error: " + err));
+      }
+    })
+    .catch((err) => res.send(err));
+});
+
+app.post("/users/login", (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username }).then((foundUser) => {
+    if (!foundUser) {
+      res.send("User does not exist.");
+    } else {
+      bcrypt.compare(password, foundUser.password).then(function (result) {
+        if (result) {
+          const token = jwt.sign(
+            { username: username },
+            process.env.SECRET_KEY
+          );
+          //storing the generated token as a cookie so to send the JWT along with required routes.
+          res.cookie("token", token);
+          res.json({ token }); //get hold of token here and use it as authorization header value for further requests
         } else {
-            //if no such user exists, salt and hash the input password, and store the hash, email, username.
-            bcrypt.hash(password, saltRounds)
-            .then((hash)=>{
-                    const newUser = new User({
-                        username: username,
-                        email: email,
-                        password: hash
-                    })
-                    newUser.save().then(()=>{
-                        new Posts({username: username}).save()
-                        .then(()=>{res.send("Successfully saved user")})
-                        .catch(()=>{res.send("Error occurred while registering user.")})
-                        
-                    })
-                    .catch(()=>{res.send("Some error occurred while saving user")})
-                })
-            .catch(err => res.send("Error: "+err));
+          res.send("Incorrect password!");
         }
-    })
-    .catch(err=>res.send(err));
-})
-
-app.post("/users/login", (req, res)=>{
-    const {username, password} = req.body;
-    User.findOne({username})
-    .then((foundUser)=>{
-        if (!foundUser){
-            res.send("User does not exist.");
-        }
-        else{
-            bcrypt.compare(password, foundUser.password).then(function(result) {
-                if (result){
-                    const token = jwt.sign({ username: username }, process.env.SECRET_KEY);
-                    //storing the generated token as a cookie so to send the JWT along with required routes.
-                    res.cookie("token",token);
-                    res.json({ token }); //get hold of token here and use it as authorization header value for further requests
-                } else {
-                    res.send("Incorrect password!");
-                }
-            });  
-        }
-    })
-
-})
+      });
+    }
+  });
+});
 
 //visit the profile of another user.
-app.get("/users/:username", (req, res)=>{
-    const username = req.params.username;
-    User.findOne({username: username})
-    .then((foundUser)=>{
-        console.log(foundUser);
-        if (foundUser){
-            res.send({
-                ...foundUser._doc,
-                password: "",
-                email: ""
-            })
-        }
-        else{
-            console.log("User doesn't exist.");
-            res.send();
-        }
-    })
-})
+app.get("/users/:username", (req, res) => {
+  const username = req.params.username;
+  User.findOne({ username: username }).then((foundUser) => {
+    console.log(foundUser);
+    if (foundUser) {
+      res.send({
+        ...foundUser._doc,
+        password: "",
+        email: "",
+      });
+    } else {
+      console.log("User doesn't exist.");
+      res.send();
+    }
+  });
+});
 
+app.get("/users/:username/posts", (req, res) => {
+  const username = req.params.username;
+  Posts.findOne({ username: username })
+    .then((foundUser) => {
+      res.json(foundUser.posts);
+    })
+    .catch(() => {
+      res.send("Error occurred while fetching posts.");
+    });
+});
 
 //a post request on this route when the user is logged in will give him all his info.
 //does not require any form input.
-app.post('/users/profile', authenticateToken, (req, res)=>{
-    User.findOne({username: req.username})
-    .then((foundUser)=>{
-        if (foundUser){
-            res.json(foundUser);
-        } else {
-            res.send("Unable to find user.");
-        }
-    }).catch(()=>{res.send("Error retrieving profile.")})
-})
+app.post("/users/profile", authenticateToken, (req, res) => {
+  User.findOne({ username: req.username })
+    .then((foundUser) => {
+      if (foundUser) {
+        res.json(foundUser);
+      } else {
+        res.send("Unable to find user.");
+      }
+    })
+    .catch(() => {
+      res.send("Error retrieving profile.");
+    });
+});
 
 //logged in user creating a new post.
-app.post('/users/posts/', authenticateToken, upload.single('file'), (req, res) => {
+app.post(
+  "/users/posts/",
+  authenticateToken,
+  upload.single("file"),
+  (req, res) => {
     // Access the filename of the uploaded file
-    const uploadedFileName = "./uploads/"+req.file.filename;
+    const uploadedFileName = "./uploads/" + req.file.filename;
     const caption = req.body.caption;
     const post = new singlePost({
-        imgPath: uploadedFileName,
-        caption: caption
-    })
+      imgPath: uploadedFileName,
+      caption: caption,
+    });
     Posts.findOneAndUpdate(
-        { username: req.username }, 
-        { $push: { posts: post  } }
-    ).then(()=>{
+      { username: req.username },
+      { $push: { posts: post } }
+    )
+      .then(() => {
         res.json({
-            message: "Successfully created a new post!",
-            caption: caption,
-            filename: uploadedFileName
+          message: "Successfully created a new post!",
+          caption: caption,
+          filename: uploadedFileName,
         });
-    }).catch(()=>{res.send("Error occurred while creating new post.")})
-
-  });
+      })
+      .catch(() => {
+        res.send("Error occurred while creating new post.");
+      });
+  }
+);
 
 //logged in user trying to update his username.
 //updating username will make the current AuthToken invalid, so we need to LOGIN again.
-app.patch('/users/profile/username', authenticateToken, (req, res) => {
-      User.findOne({username: req.username})
-      .then((user)=>{
-        const {username} = req.body; //what the user wants to change the username to.
-        //checking if there the new username is already used by some other user or not.
-        User.findOne({username: username})
-        .then((newUserExists)=>{
-            if (newUserExists){
-                res.send("A user with this username already exists.");
-            }
-            else{
-                User.updateOne({username: user.username}, {username: username})
-                .then(()=>{
-                    Posts.updateOne({username: user.username}, {username: username})
-                    .then(()=>{res.send("Successfully updated username! Please login again." )})
-                    .catch(()=>{res.send("Error updating username")})
-                    
-                }).catch(()=>{res.send("Error updating username")})
-            }
-        }).catch((err)=>{res.send("Error finding user.")})
-      })
-    .catch ((error)=> {
-      res.status(500).json({ error: 'Error retrieving user profile' });
+app.patch("/users/profile/username", authenticateToken, (req, res) => {
+  User.findOne({ username: req.username })
+    .then((user) => {
+      const { username } = req.body; //what the user wants to change the username to.
+      //checking if there the new username is already used by some other user or not.
+      User.findOne({ username: username })
+        .then((newUserExists) => {
+          if (newUserExists) {
+            res.send("A user with this username already exists.");
+          } else {
+            User.updateOne({ username: user.username }, { username: username })
+              .then(() => {
+                Posts.updateOne(
+                  { username: user.username },
+                  { username: username }
+                )
+                  .then(() => {
+                    res.send(
+                      "Successfully updated username! Please login again."
+                    );
+                  })
+                  .catch(() => {
+                    res.send("Error updating username");
+                  });
+              })
+              .catch(() => {
+                res.send("Error updating username");
+              });
+          }
+        })
+        .catch((err) => {
+          res.send("Error finding user.");
+        });
     })
-  });
-
-
-  app.patch('/users/profile/password', authenticateToken, (req, res) => {
-    User.findOne({username: req.username})
-    .then((user)=>{
-        console.log(user);
-      const {password} = req.body;
-      bcrypt.compare(password, user.password).then(function(result) {
-        if (result){
-            res.send("Can't update to the same password.");
-        } else {
-            bcrypt.hash(password, saltRounds)
-            .then((hash)=>{      
-                User.updateOne({username: user.username}, {password: hash})
-                .then(()=>{res.send("Successfully updated user's password.")})
-                .catch(()=>{res.send("Error occurred while updated password.")})
-            })
-            .catch(()=>{res.send("Error occurred while comparing passwords.")});
-        }
+    .catch((error) => {
+      res.status(500).json({ error: "Error retrieving user profile" });
     });
-    })
-  .catch ((error)=> {
-    res.status(500).json({ error: 'Error retrieving user profile' });
-  })
 });
 
-app.patch('/users/profile/bio', authenticateToken, (req, res) => {
-    User.findOne({username: req.username})
-    .then((user)=>{
-        console.log(user);
-      const {bio} = req.body;
-      User.updateOne({username: user.username}, {bio: bio})
-      .then(()=>{res.send("Successfully updated bio!")})
-      .catch(()=>{res.send("Error occurred while updating bio.")})
+app.patch("/users/profile/password", authenticateToken, (req, res) => {
+  User.findOne({ username: req.username })
+    .then((user) => {
+      console.log(user);
+      const { password } = req.body;
+      bcrypt.compare(password, user.password).then(function (result) {
+        if (result) {
+          res.send("Can't update to the same password.");
+        } else {
+          bcrypt
+            .hash(password, saltRounds)
+            .then((hash) => {
+              User.updateOne({ username: user.username }, { password: hash })
+                .then(() => {
+                  res.send("Successfully updated user's password.");
+                })
+                .catch(() => {
+                  res.send("Error occurred while updated password.");
+                });
+            })
+            .catch(() => {
+              res.send("Error occurred while comparing passwords.");
+            });
+        }
+      });
     })
-  .catch ((error)=> {
-    res.status(500).json({ error: 'Error retrieving user profile' });
-  })
+    .catch((error) => {
+      res.status(500).json({ error: "Error retrieving user profile" });
+    });
+});
+
+app.patch("/users/profile/bio", authenticateToken, (req, res) => {
+  User.findOne({ username: req.username })
+    .then((user) => {
+      console.log(user);
+      const { bio } = req.body;
+      User.updateOne({ username: user.username }, { bio: bio })
+        .then(() => {
+          res.send("Successfully updated bio!");
+        })
+        .catch(() => {
+          res.send("Error occurred while updating bio.");
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Error retrieving user profile" });
+    });
 });
 
 function authenticateToken(req, res, next) {
-    const token = req.cookies.token
-    
-    if (token == null) return res.sendStatus(401)
-  
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-      if (err) {return res.sendStatus(403)}
-      req.username = user.username
-      next()
-    })
-  }
-  
+  const token = req.cookies.token;
 
-app.listen(PORT, function(){
-    console.log(`Listening on port ${PORT}`);
-})
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.username = user.username;
+    next();
+  });
+}
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
