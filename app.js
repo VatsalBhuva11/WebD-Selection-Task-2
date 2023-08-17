04d0e36376ef4ec2c23b15bfe963904944e57038
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-08-17 08:15:14 +0300

    testing deletion

--- app.js
+++ app.js
@@ -482,23 +482,19 @@ app.delete("/users/:username/:postID", authenticateToken, (req, res) => {
     const username = req.params.username;
     const postID = req.params.postID;
 
-    Posts.findOne({username: username})
-    .then((foundPost)=>{res.send(foundPost)})
-    .catch(()=>{res.send("error")});
-    // Posts.updateOne(
-    //     { username: username },
-    //     { $pull: { posts: { _id: postID } } }
-    //   )
-    //     .then(result => {
-    //       if (result.nModified > 0) {
-    //         res.send("Successfully deleted the post.");
-    //     } else {
-    //         res.send("Post not found.");
-    //       }
-    //     })
-    //     .catch(error => {
-    //       console.error('Error deleting post:', error);
-    //     });
+    // Posts.findOne({username: username})
+    // .then((foundPost)=>{res.send(foundPost)})
+    // .catch(()=>{res.send("error")});
+    Posts.updateOne(
+        { username: username },
+        { $pull: { posts: { _id: postID } } }
+      )
+        .then(result => {
+          res.send(result);
+        })
+        .catch(error => {
+          res.send('Error deleting post:', error);
+        });
 });
 
 app.get("/test", (req,res)=>{
