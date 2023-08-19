d94113cf9d876758bdc51e6ab888f8b719d7e9d3
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-08-19 15:53:13 +0300

    fixed bug

--- schemas/post_schema.js
+++ schemas/post_schema.js
@@ -1,6 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-const Post = require('./singlePost_schema')
+const Post = new Schema({
+  username: String,
+  imgPath: String,
+  caption: String,
+  date: String,
+  likes: { type: Number, default: 0 },
+  likedBy: Array,
+  dislikes: { type: Number, default: 0 },
+  dislikedBy: Array,
+  comments: Array,
+});
+
 //a new schema to store the username, email, and hash of every user.
 const socialMediaPosts = new Schema({
   username: String,
