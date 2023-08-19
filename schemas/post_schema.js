fe35558a112662b5668e12ec626e8f33442d1288
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-08-19 15:50:09 +0300

    require Post in Posts schema

--- schemas/post_schema.js
+++ schemas/post_schema.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+const Post = require('./singlePost_schema')
 //a new schema to store the username, email, and hash of every user.
 const socialMediaPosts = new Schema({
   username: String,
